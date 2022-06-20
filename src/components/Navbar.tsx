2cb6fec9900a8ba478f15b0a1f9638f797d5b37d
Aplietexe <pietropalo@icloud.com>
2022-06-20 19:41:32 -0300

    Adapt Navbar to desktop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-leaked-render */
 import { Col, Image as ImageComponent, Row } from "react-bootstrap"
 import styled from "styled-components"
 import { useCallback } from "react"
@@ -6,6 +7,7 @@ import workflowLogo from "../assets/Navbar/workflow-logo.svg"
 import loginIcon from "../assets/Navbar/login-icon.svg"
 import burgerIcon from "../assets/Navbar/burger-icon.svg"
 import type { SetState } from "../types"
+import useBreakpoints from "../hooks/useBreakpoints"
 
 import Pad from "./common/Pad"
 
@@ -23,6 +25,31 @@ const StyledImage = styled(ImageComponent)`
   cursor: pointer;
 `
 
+const NavLinks = styled(Col)`
+  justify-content: end;
+  align-items: center;
+  gap: 2rem;
+
+  padding-inline: 2rem;
+
+  a {
+    font-family: "Poppins", sans-serif;
+    font-size: 1.5rem;
+    font-weight: 500;
+    color: var(--text-color, var(--accent-dark));
+
+    text-decoration: none;
+  }
+`
+
+const ContactLink = styled.a`
+  border-radius: var(--border-radius);
+  background-color: var(--accent-light);
+  --text-color: #fff;
+
+  padding: 0.75rem 2rem;
+`
+
 type NavbarProps = Readonly<{
   setIsSidebarOpen: SetState<boolean>
 }>
@@ -32,32 +59,58 @@ const Navbar = ({ setIsSidebarOpen }: NavbarProps) => {
     setIsSidebarOpen(true)
   }, [setIsSidebarOpen])
 
+  const breakpoints = useBreakpoints()
+
+  const goBack = (
+    <Row className="mt-4 mb-3 mb-xl-4">
+      <Col
+        className="text-end text-xl-center"
+        xl={{ offset: 11, span: 1 }}
+        xs={{ offset: 10, span: 2 }}
+      >
+        <StyledLink href="#">Volver</StyledLink>
+      </Col>
+    </Row>
+  )
+
   return (
-    <Pad>
-      <Row className="mt-4 mb-3">
-        <Col className="text-end" xs={{ offset: 10, span: 2 }}>
-          <StyledLink href="#">Volver</StyledLink>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="d-flex align-items-end fs-1 ps-4 pb-2" xs={2}>
-          <ImageComponent
-            onClick={handleBurgerClick}
-            role="button"
-            src={burgerIcon}
-          />
-        </Col>
-        <Col className="d-flex justify-content-center" xs={8}>
-          <ImageComponent fluid src={workflowLogo} />
-        </Col>
-        <Col
-          className="d-flex align-items-end justify-content-end pe-4 pb-2"
-          xs={2}
-        >
-          <StyledImage fluid src={loginIcon} />
-        </Col>
-      </Row>
-    </Pad>
+    <>
+      {breakpoints.xl && goBack}
+      <Pad>
+        {!breakpoints.xl && goBack}
+        <Row>
+          <Col
+            className="d-flex align-items-end fs-1 ps-4 pb-2 d-xl-none"
+            xs={2}
+          >
+            <ImageComponent
+              onClick={handleBurgerClick}
+              role="button"
+              src={burgerIcon}
+            />
+          </Col>
+          <Col
+            className="d-flex justify-content-center justify-content-xl-start"
+            xl={5}
+            xs={8}
+          >
+            <ImageComponent fluid src={workflowLogo} />
+          </Col>
+          <Col
+            className="d-flex align-items-end justify-content-end pe-4 pb-2 d-xl-none"
+            xs={2}
+          >
+            <StyledImage fluid src={loginIcon} />
+          </Col>
+          <NavLinks className="d-none d-xl-flex" xl={7}>
+            <a href="#root">Inicio</a>
+            <a href="#services">Servicios</a>
+            <a href="#company">Nosotros</a>
+            <ContactLink href="#contact-form">Contacto</ContactLink>
+          </NavLinks>
+        </Row>
+      </Pad>
+    </>
   )
 }
 
